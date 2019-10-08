b0cd69e202e27a3640155970d0087035aa2b35d8
Viet Dinh <vietqudinh@gmail.com>
2019-10-08 12:45:07 -0700

    Add post table for testing

--- database/dummy_data.sql
+++ database/dummy_data.sql
@@ -15,3 +15,18 @@ CREATE TABLE users_v0
 INSERT INTO users_v0
 VALUES(0, 'test', 'test@gmail.com', '2019-09-05 14:29:36', 30);
 
+CREATE TABLE posts
+(
+  id INT,
+  title VARCHAR(255),
+  content TEXT,
+  at_channel VARCHAR(255),
+  created_at DATETIME,
+  created_by VARCHAR(255),
+  updated_at DATETIME,
+  points INT,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO posts VALUES(0,"test post 0","Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacinia ante sed sapien dignissim, id luctus orci lobortis.",
+					"test_channel","2019-10-05 22:29:36","tester","2019-10-05 22:29:36",4);
