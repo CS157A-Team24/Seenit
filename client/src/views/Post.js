18ca50ca90a6181d452e966071710aa7f5c447f0
Viet Dinh <vietqudinh@gmail.com>
2019-11-28 13:44:56 -0800

    Add an action to fetch a post details and  minor fixes

--- client/src/views/Post.js
+++ client/src/views/Post.js
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Grid } from '@material-ui/core';
 import styled from 'styled-components';
 
-import { requestChannelDetails } from '../actions/Channel';
+import { requestAPost } from '../actions/Post';
 
 import ChannelContainers from '../components/ChannelContainers';
 import PostContainer from '../components/PostContainer';
@@ -12,27 +12,29 @@ import CommentList from '../components/CommentList';
 
 const Post = ({ match }) => {
     const { params: { postId } } = match;
-    const posts = useSelector(state => state.post.posts);
-    const channel= useSelector(state => state.channel);
+    const post = useSelector(state => state.post);
+    const channel = useSelector(state => state.channel);
     const dispatch = useDispatch();
-    
+
     useEffect(() => {
-        dispatch(requestChannelDetails(posts.find(content => content.post.id === postId).channel.id));
-	},[dispatch,postId,posts]);
-    
+        dispatch(requestAPost(postId));
+    }, [dispatch, postId]);
+
     return (
         <FirstContainer>
             <SecondContainer>
                 <Grid container direction="row" justify="center">
                     <CenterContainer>
                         <Grid container direction="row" justify="center" alignItems="flex-start" spacing={3} style={{ marginBottom: 0 }}>
-                            <Grid item xs={9}>
-                                <PostContainer posts={posts.find(content => content.post.id === postId)}/>
-                                <CommentBox />
-                                <CommentList />
+                            <Grid item direction="column" xs={9}>
+                                <PostCommentContainer>
+                                    {post.postDetails != null && <PostContainer postDetails={post.postDetails} />}
+                                    <CommentBox style={{marginTop: "2%" }}/>
+                                    <CommentList />
+                                </PostCommentContainer>
                             </Grid>
                             <Grid item xs={3}>
-                                {!channel.isFetching && channel.channelDetails != null && <ChannelContainers />}
+                                {channel.channelDetails != null && <ChannelContainers />}
                             </Grid>
                         </Grid>
                     </CenterContainer>
@@ -46,6 +48,11 @@ const CenterContainer = styled.div`
     margin: 0 5%;
     width: 100%;
 `;
+
+const PostCommentContainer = styled.div`
+    background-color: ${props => props.theme.foreground};
+`;
+
 const FirstContainer = styled.div`
     background-color: ${props => props.theme.background};
 `;
