32b5d9c7a173e826fb6ec7a4f23ddacf0b1eecb1
chazchang <chazhchang@gmail.com>
2019-12-02 11:40:31 -0800

    separate login loginform to push props.history to nested component

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
-import { login } from '../utils/api';
-import { Link} from 'react-router-dom';
 import {Style} from './Style';
-import { ACCESS_TOKEN } from '../constants';
+import LoginForm from './LoginForm'
 
-import { Form, Input, Button, Icon, notification } from 'antd';
-const FormItem = Form.Item;
+import { Form } from 'antd';
 
 class Login extends Component {
     render() {
@@ -15,78 +12,11 @@ class Login extends Component {
             <div className="container">                
                 <div className="box">   
                     <h1 class="center">LOGIN</h1>      
-                    <AntWrappedLoginForm onLogin={this.props.onLogin} />
+                    <AntWrappedLoginForm/>
                 </div>              
             </div>
             </Style> 
         )
-    }
-}
-class LoginForm extends Component {
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();   
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                const loginRequest = Object.assign({}, values);
-                login(loginRequest)
-                .then(response => {
-                    localStorage.setItem(ACCESS_TOKEN, response.accessToken);
-                    this.props.onLogin();
-                }).catch(error => {
-                    if(error.status === 401) {
-                        notification.error({
-                            message: 'Seenit',
-                            description: 'Your Username or Password is incorrect. Please try again!'
-                        });                    
-                    } else {
-                        notification.error({
-                            message: 'Seenit',
-                            description: error.message || 'Sorry! Something went wrong. Please try again!'
-                        });                                            
-                    }
-                });
-            }
-        });
-    }
-
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        return (
-            <Form onSubmit={this.handleSubmit} className="login-form">
-                <FormItem>
-                    {getFieldDecorator('usernameOrEmail', {
-                        rules: [{ required: true, message: 'Please input your username or email!' }],
-                    })(
-                    <Input 
-                        prefix={<Icon type="user" />}
-                        size="large"
-                        name="usernameOrEmail" 
-                        placeholder="Username or Email" />    
-                    )}
-                </FormItem>
-                <FormItem>
-                {getFieldDecorator('password', {
-                    rules: [{ required: true, message: 'Please input your Password!' }],
-                })(
-                    <Input 
-                        prefix={<Icon type="lock" />}
-                        size="large"
-                        name="password" 
-                        type="password" 
-                        placeholder="Password"  />                        
-                )}
-                </FormItem>
-                <FormItem>
-                    <Button type="primary" htmlType="submit" size="large" className="login-form-button">Login</Button>
-                    Don't have an account yet? <Link to="/register">Sign up!</Link>
-                </FormItem>
-            </Form>
-        );
     }
 }
 
