6354683b98975234c5571b59ff9a10c0826af967
Viet Dinh <vietqudinh@gmail.com>
2019-12-03 13:08:22 -0800

    temporary fix for body height

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -4,7 +4,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${props => props.theme.background};
     padding-bottom: 2%;
-    height: 100%;
+    min-height: 800px;
   }
 
 `;
