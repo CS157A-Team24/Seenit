2ac4f031db0df1bfd68d09af70062f9edc2f129b
chazchang <chazhchang@gmail.com>
2019-12-04 12:55:10 -0800

    add backend password reset

---
+++ server/src/main/java/com/seenit/server/dto/PassResetDTO.java
@@ -0,0 +1,13 @@
+package com.seenit.server.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class PassResetDTO {
+
+    @NotNull
+    final private String password;
+
+}
